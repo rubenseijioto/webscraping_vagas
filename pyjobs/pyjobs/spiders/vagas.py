1db26f610d1aa9fd22ced8d2afd68b614a3d8457
rubenseijioto <45436568+rubenseijioto@users.noreply.github.com>
2018-12-01 00:26:46 -0200

    Update vagas.py

--- pyjobs/pyjobs/spiders/vagas.py
+++ pyjobs/pyjobs/spiders/vagas.py
@@ -1,28 +1,29 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Nov 26 13:11:04 2018
-
-@author: Rodrigo
+@author: Rubens
 """
 import scrapy
-
+from unicodedata import normalize
 
 class VagasSpider(scrapy.Spider):
     name = "vagas"
 
     start_urls = [
-        'https://www.vagas.com.br/vagas-de-cientista-de-dados?'
-     
+        'http://www.pyjobs.com.br/?search=cientista+de+dados'
     ]
-  
+
     def parse(self, response):
-        for vaga in response.css('div.informacoes-header'):
-            yield {
-                'vaga': vaga.css('a::attr(title)').extract_first(),
-                'nivel': vaga.css('span.nivelVaga::text').extract_first(),
-                'empresa': vaga.css('span.emprVaga::text').extract_first(),
-                'link': vaga.css('a::attr(href)').extract(),
-            }
+        for vaga in response.css('div.card-body'):
+            titulo = vaga.css('h4::text').extract_first()
+            if 'cientista' in titulo.lower():
+                titulo = normalize('NFKD', titulo).encode('ASCII', 'ignore').decode('ASCII')
+                local = vaga.css('h6::text').extract_first()
+                local = normalize('NFKD', local).encode('ASCII', 'ignore').decode('ASCII')
+                yield {
+                   'vaga': titulo,
+                   'localizacao': local,
+                   'link': 'http://www.pyjobs.com.br' + vaga.css('a::attr(href)').extract()[0],
+                      }
+
             
 
    
